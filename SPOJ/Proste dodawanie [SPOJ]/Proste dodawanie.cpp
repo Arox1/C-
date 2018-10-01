3ecd50639c33e532e8e6c17683b084d432d938b4
Grzegorz Tomaszewski <41800726+Arox1@users.noreply.github.com>
2018-10-01 17:55:16 +0200

    Proste dodawanie [SPOJ]
    W pierwszym wierszu znajduje się liczba t testów (0 < t < 100) Każdy test opisany jest w następujący sposób. W pierwszym wierszu dana jest liczba n - liczba liczb do zsumowania. Następnie podanych jest n liczb pooddzielanych spacją.

---
+++ SPOJ/Proste dodawanie [SPOJ]/Proste dodawanie.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l_testow;
+    int ile_liczb;
+    cout << "Podaj liczbe testow: ";
+    cin >> l_testow;
+    cout << "Liczba testow: " << l_testow << endl;
+    for(int u=0; u<l_testow; u++)
+    {
+        cout << "Podaj ilosc liczb do zsumowania: ";
+        cin >> ile_liczb;
+        cout << "Ilosc liczb do zsumowania: " << ile_liczb << endl;
+        int tab[ile_liczb];
+        int suma=0;
+        for(int i=0; i<ile_liczb; i++)
+        {
+          cout << "Podaj " << i+1 << " liczbe: ";
+          cin >> tab[i] ;
+          suma=suma+tab[i];
+        }
+        cout << "Suma podanych liczb wynosi: " << suma << endl;
+        cout << "-------------------------------------" << endl;
+
+    }
+    return 0;
+}
